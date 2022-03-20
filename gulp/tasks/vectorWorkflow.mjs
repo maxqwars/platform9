a86309a9ff153330e3010d68f331565accde0ff6
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-03-20 11:32:07 +0700

    added: vector images processing

---
+++ gulp/tasks/vectorWorkflow.mjs
@@ -0,0 +1,25 @@
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import newer from "gulp-newer";
+import paths from "../paths.mjs";
+import bs from "browser-sync";
+import svgmin from "gulp-svgmin";
+
+export default () => {
+  return gulp
+    .src(paths.src.svg)
+    .pipe(
+      plumber(
+        notify.onError({
+          title: "Vector image workflow",
+          message: "<%= error.message %>",
+        })
+      )
+    )
+    .pipe(newer(paths.dest.images))
+    .pipe(svgmin())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(paths.dest.images))
+    .pipe(bs.stream());
+};
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,24 +1,32 @@
 import gulp from "gulp";
+import paths from "./gulp/paths.mjs";
+
+/* Tasks */
 import startBrowserSync from "./gulp/tasks/startBrowserSync.mjs";
 import HTMLWorkflow from "./gulp/tasks/HTMLWorkflow.mjs";
 import stylesWorkflow from "./gulp/tasks/stylesWorkflow.mjs";
 import removeBuild from "./gulp/tasks/removeBuild.mjs";
 import scriptsWorkflow from "./gulp/tasks/scriptsWorkflow.mjs";
 import imagesWorkflow from "./gulp/tasks/imagesWorkflow.mjs";
-import paths from "./gulp/paths.mjs";
+import vectorWorkflow from "./gulp/tasks/vectorWorkflow.mjs";
 
+/* Watcher */
 const watcher = () => {
   gulp.watch(paths.watch.pages, HTMLWorkflow);
   gulp.watch(paths.watch.scss, stylesWorkflow);
   gulp.watch(paths.watch.js, scriptsWorkflow);
   gulp.watch(paths.watch.images, imagesWorkflow);
+  gulp.watch(paths.watch.svg, vectorWorkflow);
 };
 
+/* Workflow */
 const workflows = gulp.parallel(
   HTMLWorkflow,
   stylesWorkflow,
   scriptsWorkflow,
-  imagesWorkflow
+  imagesWorkflow,
+  vectorWorkflow
 );
 
+/* Default task */
 export default gulp.series(removeBuild, workflows, startBrowserSync, watcher);
