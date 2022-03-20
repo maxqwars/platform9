bcc97ad9556e4335dc00987f06c1d5fafad5e456
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-03-20 11:20:14 +0700

    added: raster images processing

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,14 +4,21 @@ import HTMLWorkflow from "./gulp/tasks/HTMLWorkflow.mjs";
 import stylesWorkflow from "./gulp/tasks/stylesWorkflow.mjs";
 import removeBuild from "./gulp/tasks/removeBuild.mjs";
 import scriptsWorkflow from "./gulp/tasks/scriptsWorkflow.mjs";
+import imagesWorkflow from "./gulp/tasks/imagesWorkflow.mjs";
 import paths from "./gulp/paths.mjs";
 
 const watcher = () => {
   gulp.watch(paths.watch.pages, HTMLWorkflow);
   gulp.watch(paths.watch.scss, stylesWorkflow);
   gulp.watch(paths.watch.js, scriptsWorkflow);
+  gulp.watch(paths.watch.images, imagesWorkflow);
 };
 
-const workflows = gulp.parallel(HTMLWorkflow, stylesWorkflow, scriptsWorkflow);
+const workflows = gulp.parallel(
+  HTMLWorkflow,
+  stylesWorkflow,
+  scriptsWorkflow,
+  imagesWorkflow
+);
 
 export default gulp.series(removeBuild, workflows, startBrowserSync, watcher);
